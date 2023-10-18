2c9afb6e9a70a4a38421ddc80454264ce312e6fc
Sergei Melnikov <melnikhov@gmail.com>
2023-10-18 20:11:09 +0300

    Add Arguments parser.

--- DorevConsoleApp/ArgumentsParser.cs
+++ DorevConsoleApp/ArgumentsParser.cs
@@ -1,6 +1,28 @@
+using System.Diagnostics;
+
 namespace DorevConsoleApp;
 
-public class ArgumentsParser
+public static class ArgumentsParser
 {
-    
+    public static Arguments GetArguments(string[] args)
+    {
+        if(args.Length < 1)
+            throw new ArgumentException("Not enough arguments.");
+
+        if(args.Length == 1)
+            if (args[0] == "-a" || args[0] == "-b" || args[0] == "-e")
+                throw new ArgumentException( "Option requires an argument.");
+            else
+                return new Arguments(args[0]);
+
+        if(args.Length >= 2)
+            if (args[0] == "-a" || args[0] == "-b" || args[0] == "-e")
+                return new Arguments(args[1], args[0]);
+            else if(args[0][0] == '-')
+                throw new ArgumentException($"Illegal option: {args[0]}");
+            else
+                return new Arguments(args[0]);
+
+        throw new UnreachableException();
+    }
 }
