4331f0fffd58ef41ad2be73a7a74fd5f0acc2bc1
Sergei Melnikov <melnikhov@gmail.com>
2023-10-18 20:10:49 +0300

    Add Arguments class.

--- DorevConsoleApp/Arguments.cs
+++ DorevConsoleApp/Arguments.cs
@@ -2,5 +2,16 @@ namespace DorevConsoleApp;
 
 public class Arguments
 {
-    
+    public string Option { get; set; }
+    public string Word { get; set; }
+
+    public Arguments()
+    {
+    }
+
+    public Arguments(string word, string option = "-b")
+    {
+        Word = word;
+        Option = option;
+    }
 }
