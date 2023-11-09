3f490916f3b495ce47e71a7607d4ca2e74b33306
Sergei Melnikov <melnikhov@gmail.com>
2023-11-09 20:09:09 +0300

    Add try/catch section

--- DorevTelegramBot/Program.cs
+++ DorevTelegramBot/Program.cs
@@ -9,12 +9,23 @@ string token = config["Token"]!;
 using CancellationTokenSource cts = new();
 Bot bot = new Bot(connectionString, token, cts.Token);
 
-await bot.Run();
-while(true) {
-    string? o = Console.ReadLine();
-    if (o?.ToLower() == "exit" || o?.ToLower() == "quit")
+try
+{
+    await bot.Run();
+    while (true)
     {
-        cts.Cancel();
-        return;
+        string? o = Console.ReadLine();
+        if (o?.ToLower() == "exit" || o?.ToLower() == "quit")
+            return;
     }
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine($"Exception: {e.Message}");
+    Console.Error.WriteLine($"Method: {e.TargetSite}");
+    Console.Error.WriteLine($"Stack trace: {e.StackTrace}");
+}
+finally
+{
+    cts.Cancel();
 }
