2e47b144ce217a1107f5f03531be6193541e6da4
Sergei Melnikov <melnikhov@gmail.com>
2023-11-18 23:06:15 +0300

    Rename DB creation SQL file

--- DorevTelegramBot/Resources/CreateOptionsDb.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE Options (
-    User_Id LONG NOT NULL,
-    Option TEXT NOT NULL
-)
---
+++ DorevTelegramBot/Resources/CreateSettingsDb.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Settings (
+    User_Id LONG NOT NULL,
+    Option TEXT NOT NULL,
+    Feedback TEXT NULL
+)
